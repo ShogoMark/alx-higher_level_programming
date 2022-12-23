23cdb1b799e33a68f8dbfc781cb70434aefa2393
ShogoMark <trigmark87@gmail.com>
2022-12-23 23:59:45 +0100

    task1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""No modle imported"""
+
+
+class Square:
+    """Defines a class: Square with a private attribute: size"""
+
+    def __init__(self, size):
+        self.__size = size
+
