bb593b463471e0c04d7f8a367b657d5e1225e620
ShogoMark <trigmark87@gmail.com>
2022-12-24 00:19:09 +0100

    task2

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -4,13 +4,11 @@
 
 class Square:
     """Defines a class: Square with a private attribute: size and checks Errors"""
-    def __init__(self, size=0):
 
+    def __init__(self, size=0):
         if isinstance(size, int):
             self.__size = size
         else:
             raise TypeError("size must be an integer")
-        if size >= 0
-            self.__size = size
-        else:
+        if size < 0
             raise ValueError("size must be >= 0")
