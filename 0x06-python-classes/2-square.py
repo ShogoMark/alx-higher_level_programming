64dec185a2740d22fc4f5b6cd39f6f0d1c36d429
ShogoMark <trigmark87@gmail.com>
2022-12-24 00:22:10 +0100

    task2

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -10,5 +10,5 @@ class Square:
             self.__size = size
         else:
             raise TypeError("size must be an integer")
-        if size < 0
+        if size < 0:
             raise ValueError("size must be >= 0")
