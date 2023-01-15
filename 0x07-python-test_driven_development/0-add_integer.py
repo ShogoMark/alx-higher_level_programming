99d49439425b34b1826f7e2e143d818ac19c29ad
ShogoMark <trigmark87@gmail.com>
2023-01-15 18:21:58 +0100

    task0

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -13,11 +13,10 @@ def add_integer(a, b=98):
         TypeError: if either a or b is not an int or float
 
     """
-    if not isinstance (a, (int, float)):
+    if not isinstance(a, (int, float)):
         raise TypeError('a must be an integer')
 
-    if not isinstance (b, (int, float)):
+    if not isinstance(b, (int, float)):
         raise TypeError('b must be an integer')
 
     return int(a) + int(b)
-
