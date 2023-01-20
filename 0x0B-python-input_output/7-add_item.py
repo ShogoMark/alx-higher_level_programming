05a26886c5477c6779e8be1507a36a6e3d18d243
ShogoMark <trigmark87@gmail.com>
2023-01-20 16:52:40 +0100

    task7

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -9,9 +9,10 @@ load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
 
 filename = "add_item.json"
 
-my_list = load_from_json_file(filename)
-
-my_list = []
+try:
+    my_list = load_from_json_file(filename)
+except FileNotFoundError:
+    my_list = []
 
 for arg in argv[1:]:
     my_list.append(arg)
