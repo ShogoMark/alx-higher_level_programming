1ef1e0d2c862334f0f0b8c79fb31b9f822a6759f
ShogoMark <trigmark87@gmail.com>
2023-01-20 11:42:39 +0100

    task0

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""A function that reads a text file"""
+
+
+def read_file(filename=""):
+    """Reads a text file"""
+    filename = input()
+
+    with open("filename") as f:
+        for line in f:
+            file = f.read()
+    print(file)
