54ba9e69754b2ff1638867632cdbf4b8ec59c30f
ShogoMark <trigmark87@gmail.com>
2023-01-20 23:03:13 +0100

    task8

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-"""a function that returns the dictionary description with simple data structure"""
+"""a function that returns the dictionary description"""
+
 
 def class_to_json(obj):
     """returns the dict description"""
