335e403fc38cc27934419aa23b92d25f78dcffd9
ShogoMark <trigmark87@gmail.com>
2023-01-20 23:26:42 +0100

    task10

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""retrieves a dictionary representation of a Student"""
+
+
+class Student:
+    def __init__(self, first_name, last_name, age):
+        """instantiates the class attributes"""
+
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        return list(attrs.values())
