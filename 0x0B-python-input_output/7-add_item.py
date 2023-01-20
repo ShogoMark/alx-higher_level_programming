23ce14bfa3ef051483b74aa897da3571e38afa7d
ShogoMark <trigmark87@gmail.com>
2023-01-20 15:41:51 +0100

    task7

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -7,9 +7,11 @@ load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
 
 filename = "add_item.json"
 
-my_list[] = load_from_json_file(filename)
+my_list = load_from_json_file(filename)
+
+my_list = []
 
 for arg in argv[1:]:
-    my_list[].append(arg)
+    my_list.append(arg)
 
 save_to_json_file(my_list[], filename)
