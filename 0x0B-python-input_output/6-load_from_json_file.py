8626121b18cc7520f851aeb18cbad77b3fe9ae7a
ShogoMark <trigmark87@gmail.com>
2023-01-20 14:42:18 +0100

    task6

---
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""A function that creates object from JSON file"""
+
+import json
+
+
+def load_from_json_file(filename):
+    """create object from a JSON file"""
+
+    return json.loads(filename)
