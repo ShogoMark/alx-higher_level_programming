aa59e1b4359430ebd563db0d9af8df69771aec33
ShogoMark <trigmark87@gmail.com>
2023-01-20 13:33:52 +0100

    task1

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -5,6 +5,6 @@
 def write_file(filename="", text=""):
     """writes to a file"""
 
-    with open(filename, "r+", encoding=utf-8) as f:
+    with open(filename, 'r+', encoding=utf-8) as f:
         file = f.write(text)
         return len(file)
