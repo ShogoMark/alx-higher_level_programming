b0e2aa180c6094ccd070201063aad1b97153c54c
ShogoMark <trigmark87@gmail.com>
2023-01-25 12:37:15 +0100

    task7

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,17 +1,15 @@
 #!/usr/bin/python3
 """Scripts that adds all arguments and saves on a list
 """
-
-import json
 import sys
 
 save_to_json_file = __import__("5-save-to_json_file").save_to_json_file
 load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
 
-my_list = [load_from_json_file(add_item.json)]
+my_list = load_from_json_file("add_item.json")
 
 
 for arg in argv[1:]:
     my_list.append(arg)
 
-save_to_json_file(my_list, add_item.json)
+save_to_json_file(my_list, "add_item.json")
