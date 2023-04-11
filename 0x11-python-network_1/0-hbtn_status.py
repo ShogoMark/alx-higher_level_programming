6236611ce13a85f78c0daf5c7d62be81cfea824d
ShogoMark <trigmark87@gmail.com>
2023-04-11 16:12:21 +0000

    tass0

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -5,11 +5,12 @@ import urllib.request
 
 
 if __name__ == "__main__":
+"""Function not to run if not in main"""
 
-#Opening the URL
 	with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
-#reading the URL
+	"""Opening the URL"""
 		res = response.read()
+		"""Reading the URL"""
 print("Body response:")
 print("\t- type: {}".format((type(res)))
 print("\t- content: {}".format(res))
