9f45ec1dafcbd4a395450ac64becacf4f94e01c8
ShogoMark <trigmark87@gmail.com>
2023-04-11 09:33:35 +0000

    taskeds9

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #A script that makes a request, responds with a string
-curl -X -sL PUT -H 0.0.0.0:5000/catch_me -d "You got me!"
+curl -X GET 0.0.0.0:5000/catch_me && ["$(curl -X GET 0.0.0.0:5000/catch_me)" = "You got me!"] 
