d6ced2542f08cc0a3f1a2118a16e7f6abd69e77d
ShogoMark <trigmark87@gmail.com>
2023-04-11 14:39:12 +0000

    taskk0

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -3,10 +3,14 @@
 
 import urllib.request
 
+
 if __name__ == "__main__":
 
 #Opening the URL
 	with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
 #reading the URL
 		res = response.read()
-print("	- {}".format(res))
+print("Body response:")
+print("\t- type: {}".format((type(res)))
+print("\t- content: {}".format(res))
+print("\t- utf8 content: {}".format(res.decode('utf-8'))
