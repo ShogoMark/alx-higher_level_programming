72ff6f9be8a0b9891ed284ea4af4985d874ea30f
ShogoMark <trigmark87@gmail.com>
2023-05-03 23:34:03 +0000

    task0

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,17 +1,16 @@
 #!/usr/bin/python3
-#Python scripts that fetches url
+"""fetches a URL using urllib"""
 
 import urllib.request
 
 
 if __name__ == "__main__":
-"""Function not to run if not in main"""
+
 
 	with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
-	"""Opening the URL"""
 		res = response.read()
-		"""Reading the URL"""
-print("Body response:")
-print("\t- type: {}".format((type(res)))
-print("\t- content: {}".format(res))
-print("\t- utf8 content: {}".format(res.decode('utf-8'))
+
+		print("Body response:")
+		print("\t- type: {}".format((type(res)))
+		print("\t- content: {}".format(res))
+		print("\t- utf8 content: {}".format(res.decode('utf-8'))
